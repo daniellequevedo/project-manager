8e3c46b76275006375afb61279c8cf3df66853f9
Danielle Quevedo <quevedo.danielle2@gmail.com>
2018-11-21 16:22:07 -0600

    added tasks header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import TasksPage from './containers/TasksPage';
 
 class App extends Component {
   render() {
     return (
-      <h1>Hello World!</h1>
+      <TasksPage />
     );
   }
 }
