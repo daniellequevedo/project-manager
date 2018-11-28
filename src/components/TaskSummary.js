9231e9fec19576b50224d4f436146433f348735c
Danielle Quevedo <quevedo.danielle2@gmail.com>
2018-11-28 17:07:50 -0600

    started task details expansion window

--- src/components/TaskSummary.js
+++ src/components/TaskSummary.js
@@ -1,11 +1,20 @@
 import React from 'react';
 
-const TaskSummary = ({task_name, task_due_date, project_name}) => {
+const TaskSummary = ({task: {
+        task_id, 
+        task_name, 
+        task_due_date
+    }, 
+    project_id,
+    project_name, 
+    handleDisplayTaskDetails}) => {
+
     return (
         <li className="list-item">
             <span>{task_name}</span>
             <span>{task_due_date}</span>
             <span>{project_name}</span>
+            <button onClick={() => handleDisplayTaskDetails(task_id, project_id)}>></button>
         </li>
     );
 }
--- src/containers/TasksPage.js
+++ src/containers/TasksPage.js
@@ -1,30 +1,59 @@
-import React from 'react';
+import React, { Component } from 'react';
 import data from '../data/data';
 import TaskSummary from '../components/TaskSummary';
+import TaskDetails from '../components/TaskDetails';
 
-function TasksPage() {
-    return (
-        <React.Fragment>
-            <div className="table-headers">
-                <h1>Tasks</h1>
-                <span>Due Date</span>
-                <span>Project Name</span>
-            </div>
-            <ul className="list">
-                {data.map( (project) => {
-                    return project.project_tasks.map( (task) => {
-                        return <TaskSummary 
-                            key={task.task_id} 
-                            task_name={task.task_name} 
-                            task_due_date={task.task_due_date} 
-                            project_name={project.project_name}
-                        />
-                    })
+class TasksPage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            selectedTask: null
+        }
+    }   
+    handleDisplayTaskDetails = (task_id, project_id) => {        
+        let selectedProject = data.filter( (project) => {
+            return project.project_id === project_id;
+        })[0];
+        let selectedTask = selectedProject.project_tasks.filter( (task) => {
+            return task.task_id === task_id;
+        })[0];
+        console.log("selectedProject: ", selectedProject, "selectedTask: ", selectedTask);
+        
+        if (selectedTask === this.state.selectedTask) {
+            this.setState({selectedTask: null});
+        } else {
+            this.setState({selectedTask});
+        }
+ 
+    }
 
-                })}
-            </ul>
-        </React.Fragment>
-    );
+    render() {
+        return (
+            <React.Fragment>
+                <div className="table-headers">
+                    <h1>Tasks</h1>
+                    <span>Due Date</span>
+                    <span>Project Name</span>
+                </div>
+                <ul className="list">
+                    {data.map( (project) => {
+                        return project.project_tasks.map( (task) => {
+                            return <TaskSummary 
+                                key={task.task_id} 
+                                task={task} 
+                                project_name={project.project_name}
+                                project_id={project.project_id}
+                                handleDisplayTaskDetails={this.handleDisplayTaskDetails}
+                            />
+                        })
+                    })}
+                </ul>
+                {console.log(this.state.selectedTask)}
+
+                {this.state.selectedTask && <TaskDetails selectedTask={this.state.selectedTask} />}
+            </React.Fragment>
+        );
+    }
 }
 
 export default TasksPage;
