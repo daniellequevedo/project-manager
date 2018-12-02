2244fad8c65b4e069ccf738ee13f11802799abd3
Danielle Quevedo <quevedo.danielle2@gmail.com>
2018-12-01 21:38:44 -0600

    details sidebar styling

--- src/components/TaskDetails.js
+++ src/components/TaskDetails.js
@@ -4,6 +4,39 @@ const TaskDetails = (props) => {
     return (
         <div className="details-sidebar">
             <h3>{props.selectedTask.task_name}</h3>
+            <span>Environment - Mac</span>
+            <div className="item-detail-group">
+                <div className="item-detail">
+                    <span className="item-detail-label">Status</span>
+                    <span className="item-detail-value in-process">In Process</span>
+                </div>
+            </div>
+            <div className="item-detail-group">
+                <div className="item-detail">
+                    <span className="item-detail-label">Due Date</span>
+                    <span className="item-detail-value">01-07-19</span>
+                </div>
+                <div className="item-detail">
+                    <span className="item-detail-label">Actual End Date:</span>
+                    <span className="item-detail-value">01-07-20</span>
+                </div>
+            </div>
+            <div className="item-detail-group">
+                <div className="item-detail">
+                    <span className="item-detail-label">Assigned To:</span>
+                    <span className="item-detail-value">Hatem</span>
+                </div>
+                <div className="item-detail">
+                    <span className="item-detail-label">Assigned By:</span>
+                    <span className="item-detail-value">Hal</span>
+                </div>
+            </div>
+            <div className="item-detail-group">
+                <div className="item-detail full-width">
+                    <span className="item-detail-label">Description:</span>
+                    <span className="item-detail-value">Should be a quick intro</span>
+                </div>
+            </div>
         </div>
     );
 }
