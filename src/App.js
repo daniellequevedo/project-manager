91654f84aeba1eb6a6b4625b34b2c52105ec2440
Danielle Quevedo <quevedo.danielle2@gmail.com>
2018-12-05 20:40:29 -0600

    connected App to store

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { Switch, Route, BrowserRouter, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+
 import './App.css';
 import Header from './components/Header';
 import ProjectsPage from './containers/ProjectsPage';
@@ -24,4 +26,10 @@ class App extends Component {
   }
 }
 
-export default App;
+// export default App;
+
+// the connect function's job is to hook up the store to the app.
+// the store is being passed to the connect function.
+// connect will then return a function.
+// we then call that function and pass it the parameter App
+export default connect( store => ({store}) )(App);
