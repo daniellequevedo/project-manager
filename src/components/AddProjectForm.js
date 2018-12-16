0514b5bf235657aed66fd2490b782d3c4928ce09
Danielle Quevedo <quevedo.danielle2@gmail.com>
2018-12-16 09:16:45 -0600

    changed AddProjectForm to class based

--- src/components/AddProjectForm.js
+++ src/components/AddProjectForm.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
 import uuidv4 from 'uuid/v4';
 
-const AddProjectForm = (props) => {
+class AddProjectForm extends Component {
+// const AddProjectForm = (props) => {
 
-    let handleSubmit = e => {
+    constructor(props) {
+        super(props);
+        this.state = {
+            project_name: ''
+        }
+    }
+
+    handleSubmit = e => {
         // console.log("e.target is: ", e.target);
         // console.log('hello!')
         e.preventDefault();
@@ -12,44 +20,46 @@ const AddProjectForm = (props) => {
             project_id: uuidv4(),
             project_name: document.getElementById("project-name").value,
         };
-        props.handleAddProjectSave(project);
-        handleReset();
+        this.props.handleAddProjectSave(project);
+        this.handleReset();
     }
 
-    let handleReset = () => {
+    handleReset = () => {
         document.getElementById("project-name").value = '';
     }
 
-    return (
-        <React.Fragment>
-            <div id="target-add-project" className="target"></div>
-            <div id="modal-add-project" className="modal">
-                <div className="modal-content">
-                    <h2>Add Project</h2>
-                    <form onSubmit={handleSubmit}>
-                        <div className="item-detail">
-                            <label className="item-detail-label">Project Name:</label>
-                            <div className="item-detail-value">
-                                <input 
-                                    id="project-name"
-                                    type="text" 
-                                    className="item-detail-input" 
-                                />
+    render() {
+        return (
+            <React.Fragment>
+                <div id="target-add-project" className="target"></div>
+                <div id="modal-add-project" className="modal">
+                    <div className="modal-content">
+                        <h2>Add Project</h2>
+                        <form onSubmit={this.handleSubmit}>
+                            <div className="item-detail">
+                                <label className="item-detail-label">Project Name:</label>
+                                <div className="item-detail-value">
+                                    <input 
+                                        id="project-name"
+                                        type="text" 
+                                        className="item-detail-input" 
+                                    />
+                                </div>
                             </div>
-                        </div>
-                        <div className="modal-footer">
-                            <a href="/projects" className="button">Cancel</a>
-                            <button
-                                type="submit"
-                            >
-                                Add
-                            </button>
-                        </div>
-                    </form>
+                            <div className="modal-footer">
+                                <a href="/projects" className="button">Cancel</a>
+                                <button
+                                    type="submit"
+                                >
+                                    Add
+                                </button>
+                            </div>
+                        </form>
+                    </div>
                 </div>
-            </div>
-        </React.Fragment>
-    );
+            </React.Fragment>
+        );
+    }
 }
 
 export default AddProjectForm;
